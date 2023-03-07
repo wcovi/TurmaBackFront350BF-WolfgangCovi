6c5778c1f48de3eabc577ffd93f66c188b9faac9
Wolfgang F. Covi <wcovi@yahoo.com.br>
2023-03-07 20:29:40 -0300

    Adicionada a classe Automovel

--- Classe/Classe/main.swift
+++ Classe/Classe/main.swift
@@ -7,5 +7,26 @@
 
 import Foundation
 
-print("Hello, World!")
+// OBJETO
+// CARACTERISTICAS DE UM OBJETO -> ATRIBUTOS OU PROPRIEDADES
+// AÇÕES = FUNÇÃO / METODO
 
+// Para criar uma classe sempre começamos com a palavra reservada "class" e em seguida colocamos o seu nome que sempre deve começar com a primeira letra maiusculo
+
+class Automovel {
+    
+    var cor: String = "Preto"
+    var numeroDePortas: Int = 2
+    var marca: String = "Honda"
+    var eEletrico: Bool = false
+    var quantidadeDePassageiros: Int = 5
+    
+    func ligarFarol() {
+        print("O Farol está ligado")
+    }
+    
+    func ligarOCarro() {
+        print("O carro está ligado")
+    }
+    
+}
