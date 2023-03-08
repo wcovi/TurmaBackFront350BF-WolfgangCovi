f0c6003fe461c60557d42f88209a4537eb01ca0d
Wolfgang F. Covi <wcovi@yahoo.com.br>
2023-03-07 22:54:55 -0300

    explicação sobre classes pre-definidas e construtores

--- Classe/Classe/main.swift
+++ Classe/Classe/main.swift
@@ -80,3 +80,60 @@ class Mother {
         print("Nia sentou")
     }
 }
+
+// Dar vida ao objeto -> quando geramos a instancia -> ()
+var meuCarro: Automovel = Automovel()
+
+
+// Construtores
+// Criamos um construtor com a palavra reservada init e com isso indicamos TODOS OS VALORES DAS VARIAVEIS OU CONSTANTES QUE NÃO SE INICIAM COM VALOR
+
+class Pessoa {
+    
+    var nome: String
+    var altura: Double
+    var peso: Double
+    var tamanhoCalcado: Int
+    
+    
+    init(nome: String, altura: Double, peso: Double, tamanhoCalcado: Int) {
+        self.nome = nome
+        self.altura = altura
+        self.peso = peso
+        self.tamanhoCalcado = tamanhoCalcado
+    }
+}
+
+var caio: Pessoa = Pessoa(nome: "Caio", altura: 1.84, peso: 84, tamanhoCalcado: 44)
+var matheus: Pessoa = Pessoa(nome: "Matheus", altura: 1.65, peso: 69, tamanhoCalcado: 40)
+//print(caio.nome)
+//print(caio.altura)
+//print(caio.peso)
+
+//  Referencia uma das principais caracteristicas de uma classe
+
+print(caio.nome)
+print(matheus.nome)
+
+caio.nome = "Caio Fabrini"
+matheus.nome = "Matheus Souza"
+
+print(caio.nome)
+print(matheus.nome)
+
+// caio é igual a matheus
+caio = matheus
+
+print(caio.nome)
+print(matheus.nome)
+
+
+caio.nome = "Alberto"
+
+print(caio.nome)
+print(matheus.nome)
+
+matheus.nome = "Jorge"
+
+print(caio.nome)
+print(matheus.nome)
