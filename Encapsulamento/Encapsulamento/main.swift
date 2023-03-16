05db01d631056fb47cd4f23c5010fedd68478a1c
Wolfgang F. Covi <wcovi@yahoo.com.br>
2023-03-16 10:29:32 -0300

    explicacao sobre encapsulamento e polimorfismo

---
+++ Encapsulamento/Encapsulamento/main.swift
@@ -0,0 +1,42 @@
+//
+//  main.swift
+//  Encapsulamento
+//
+//  Created by Caio Fabrini on 14/03/23.
+//
+
+import Foundation
+
+// MARK: - Encapsulamento
+
+// Private -> privado, ou seja, que não tenha acesso externo
+// Public -> publico, ou seja, que tenha acesso externo
+
+class Carro {
+    
+    private var modelo: String
+    
+    init(modelo: String) {
+        self.modelo = modelo
+    }
+    
+    public func getModelo() -> String {
+        return self.modelo
+    }
+    
+    public func setModelo(modelo: String) {
+        self.modelo = modelo
+    }
+    
+}
+
+var meuCarro: Carro = Carro(modelo: "BMW 320i")
+
+print(meuCarro.getModelo())
+meuCarro.setModelo(modelo: "La Ferrari")
+print(meuCarro.getModelo())
+
+
+
+
+
---
+++ Polimorfismo/Polimorfismo/main.swift
@@ -0,0 +1,37 @@
+//
+//  main.swift
+//  Polimorfismo
+//
+//  Created by Caio Fabrini on 14/03/23.
+//
+
+import Foundation
+
+// MARK: - Polimorfismo
+
+class Centauro {
+    func totalDeDesconto(valorTotal: Double) -> Double {
+        return valorTotal * 0.3
+    }
+}
+
+class Loja1: Centauro {
+    override func totalDeDesconto(valorTotal: Double) -> Double {
+        return valorTotal * 0.1
+    }
+}
+
+class Loja2: Centauro {
+
+}
+
+
+var lojaDoCaio: Loja1 = Loja1()
+print(lojaDoCaio.totalDeDesconto(valorTotal: 1000))
+
+
+var lojaDoBruno: Loja2 = Loja2()
+print(lojaDoBruno.totalDeDesconto(valorTotal: 1000))
+
+
+
